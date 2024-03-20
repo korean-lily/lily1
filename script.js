767a643a2b9d7613dd8ceabf3122ac3d05f680d6
korean-lily <164233993+korean-lily@users.noreply.github.com>
2024-03-21 00:44:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ function searchProducts() {
     var productItems = document.querySelectorAll('.product-item');
 
     productItems.forEach(function(item) {
-        var productCode = item.querySelector('h3').textContent.toLowerCase();
+        var productCode = item.querySelector('h3').textContent.toLowerCase().trim();
 
-        if (productCode.includes(searchTerm)) {
+        if (productCode === "code: " + searchTerm) { // Exact match comparison including "Code: "
             item.style.display = 'block';
         } else {
             item.style.display = 'none';
         }
     });
 }
+
 document.getElementById('searchInput').addEventListener('keyup', function(event) {
     if (event.key === 'Enter') {
         searchProducts();
